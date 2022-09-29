cb8827f51c71faf699720a3626cb5c23841924dd
DaiSimeand <113060898+DaiSimeand@users.noreply.github.com>
2022-09-29 20:26:51 -0300

    contenido trabajo final
    Se realizo en contenido del proyecto final.

---
+++ index.js
@@ -0,0 +1,10 @@
+var edad = prompt("¿Que edad tienes?");
+
+
+if (edad >=18){
+    alert("Bienvido al sitio");
+}
+if (edad <=17){
+    alert("Eres menor de edad, entra bajo su responsabilidad");
+    
+}
